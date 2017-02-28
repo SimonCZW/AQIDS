c5715b6f544a0751b5b09289615ec0ab60212e6b
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-02-28 17:31:39 +0800

    API使用 urllib2

--- test/test_pm25in/pm25_get.py
+++ test/test_pm25in/pm25_get.py
@@ -2,28 +2,39 @@
 # -*- coding-utf8 -*-
 
 import json
-import requests
+import urllib
+import urllib2
 
-city="guangzhou"
-token="5j1znBVAsnSf5xQyNQyq"
+url = "http://www.pm25.in/api/querys/pm10.json"
+city = "guangzhou"
+token = "5j1znBVAsnSf5xQyNQyq"
 
-pm25 = requests.get(
-    'http://www.pm25.in/api/querys/pm2_5.json?city=%s&token=%s' % (city, token))
+values = {'city': city, 'token': token}
+datas = urllib.urlencode(values)
+lurl = url + '?' + datas
+req = urllib2.Request(lurl)
+
+# urllib2respose = urllib2.urlopen(lurl)
+urllib2respose = urllib2.urlopen(req)
+# print urllib2respose.headers.type
+try:
+    jdata = json.load(urllib2respose)
+    print "jdata", jdata
+except:
+    print "not jdata"
+
+# requests lib:
+# import requests
+# pm25 = requests.get(
+    # 'http://www.pm25.in/api/querys/pm2_5.json?city=%s&token=%s' % (city, token))
 
 # print "pm25 text", pm25.text
 # print "type", type(pm25)
 
 # need to be encode
-try:
-    parse_json2=json.loads(pm25.text)
-    print "pm25 json2", parse_json2
-    print "pm25 json2[0]", parse_json2[0]
-except:
-    print "pm25 not json2"
-
 # try:
-    # print "pm25.json", pm25.json
-    # pj = pm25.json
-    # print "type: pj", type(pj)
+    # parse_json2=json.loads(pm25.text)
+    # print "pm25 json2", parse_json2
+    # print "pm25 json2[0]", parse_json2[0]
 # except:
-    # pass
+    # print "pm25 not json2"
