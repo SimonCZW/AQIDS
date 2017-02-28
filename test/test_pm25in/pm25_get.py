8ba025181eb6f40a39fe58559af821e3d459cc0e
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-02-28 16:08:37 +0800

    json化 requests返回

--- test/test_pm25in/pm25_get.py
+++ test/test_pm25in/pm25_get.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
+# -*- coding-utf8 -*-
 
+import json
 import requests
 
-r = requests.get('http://www.pm25.in/api/querys/pm2_5.json?city=zhuhai&token=5j1znBVAsnSf5xQyNQyq')
+city="guangzhou"
+token="5j1znBVAsnSf5xQyNQyq"
 
-print r.text
+pm25 = requests.get(
+    'http://www.pm25.in/api/querys/pm2_5.json?city=%s&token=%s' % (city, token))
+
+# print "pm25 text", pm25.text
+# print "type", type(pm25)
+
+# need to be encode
+try:
+    parse_json2=json.loads(pm25.text)
+    print "pm25 json2", parse_json2
+    print "pm25 json2[0]", parse_json2[0]
+except:
+    print "pm25 not json2"
+
+# try:
+    # print "pm25.json", pm25.json
+    # pj = pm25.json
+    # print "type: pj", type(pj)
+# except:
+    # pass
