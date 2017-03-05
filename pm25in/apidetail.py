901cbd06821dbc29da232db42e386702d3a4a115
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-03-05 22:19:50 +0800

    新增获取station列表函数, 获取单个stations aqi数据函数

--- pm25in/apidetail.py
+++ pm25in/apidetail.py
@@ -101,13 +101,13 @@ def handle_data(city, now_time, data):
             table_name = city.capitalize() + timestamp
             insert_db(table_name, primary_keys, data)
 
-def get_api_details(api_url, city, token):
+def get_api_details_by_city(api_url, city, token):
     """
     input api, city, and token. return json data from api_url.
     """
     values = {'city': city, 'token': token}
-    datas = urllib.urlencode(values)
-    url = api_url + '?' + datas
+    params = urllib.urlencode(values)
+    url = api_url + '?' + params
     print url
     try:
         # for API data:
@@ -118,6 +118,47 @@ def get_api_details(api_url, city, token):
     except:
         print "not data from %s", url
 
+def get_api_details_by_station(api_url, station_code, token):
+    """
+    input api, station, and token. return json data from api_url.
+    """
+    values = {'station_code': station_code, 'token': token}
+    params = urllib.urlencode(values)
+    url = api_url + '?' + params
+    try:
+        station_api_details = urllib2.urlopen(url)
+        station_datas = json.load(station_api_details)
+        return station_datas
+    except:
+        print "not data from %s", url
+
+def get_station_list_by_city(api_url, city, token):
+    """
+    Return a city's stations code list.
+    original return data format:
+        {
+          "city": "广州",
+            "stations": [
+                    {
+                              "station_name": "广雅中学",
+                              "station_code": "1345A"
+                            },
+                    ...
+            ]
+        }
+    """
+    values = {'city': city, 'token': token}
+    params = urllib.urlencode(values)
+    url = api_url + '?' + params
+    station_code_list = []
+    try:
+        stations_response = urllib2.urlopen(url)
+        stations_list = json.load(stations_response)
+        for station in stations_list.get('stations'):
+            station_code_list.append(station['station_code'].encode('utf-8'))
+        return station_code_list
+    except:
+        print "not data from %s", url
 
 def main():
     # connect to database
@@ -126,16 +167,38 @@ def main():
     city = "guangzhou"
     api_url = "http://www.pm25.in/api/querys/aqi_details.json"
     token = "5j1znBVAsnSf5xQyNQyq"
-    all_datas = get_api_details(api_url, city, token)
+    # all_datas = get_api_details_by_city(api_url, city, token)
+
+    stations_code_list = get_station_list_by_city(
+        'http://www.pm25.in/api/querys/station_names.json', 'guangzhou', token)
+
+    all_datas = []
+    for station in stations_code_list:
+        station_data = get_api_details_by_station(
+            'http://www.pm25.in/api/querys/aqis_by_station.json',
+            station, token)
+        all_datas = all_datas + station_data
 
     if isinstance(all_datas, dict):
         for v in all_datas.itervalues():
             print v
         return
 
-    print "all_datas type: ", type(all_datas)
-    print all_datas
-    print
+    now_time = datetime.datetime.now().strftime("%Y%m%d%H")
+    with db.connection():
+        for data in all_datas:
+            handle_data(city, now_time, data)
+
+if __name__ == '__main__':
+    main()
+
+    # Testing for insert_db() function...
+    # db.create_engine(user='root', password='123456', database='test')
+    # testdata={'it1': 'itttttt1', 'it2': 1.5, 'it3': 123}
+    # table_name = 'TestTable'
+    # primary_keys = ['it1', 'it2']
+    # with db.connection():
+        # insert_db(table_name, primary_keys, testdata)
 
     # Testing data
     # all_datas = [{u'pm2_5': 41,
@@ -181,21 +244,3 @@ def main():
                   # u'pm10_24h': 86,
                   # u'no2': 35}
                 # ]
-
-    now_time = datetime.datetime.now().strftime("%Y%m%d%H")
-    print now_time
-    print
-    with db.connection():
-        for data in all_datas:
-            handle_data(city, now_time, data)
-
-if __name__ == '__main__':
-    main()
-
-    # Testing for insert_db() function...
-    # db.create_engine(user='root', password='123456', database='test')
-    # testdata={'it1': 'itttttt1', 'it2': 1.5, 'it3': 123}
-    # table_name = 'TestTable'
-    # primary_keys = ['it1', 'it2']
-    # with db.connection():
-        # insert_db(table_name, primary_keys, testdata)
