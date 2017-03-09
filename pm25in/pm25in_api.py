c12824f8de59c2a218b827df06ecb1cc1dbcc6e1
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-03-09 22:59:11 +0800

    重构

--- pm25in/pm25in_api.py
+++ pm25in/pm25in_api.py
@@ -2,239 +2,156 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import json
-import urllib
-import urllib2
 import datetime
-from collections import OrderedDict
 
 sys.path.append('/root/AQIDS/wrapper')
-import db
+from db import connection
+from jsonapibase import GetJsonApiBase
 
-def get_items(data):
-    """
-    return table items from data for create table.
-    """
-    _table_items = OrderedDict()
-    for key, value in data.iteritems():
-        if isinstance(key, unicode):
-            key = key.encode('utf-8')
-        if isinstance(value, int):
-            _table_items[key] = "int"
-        elif isinstance(value, float):
-            _table_items[key] = "float"
+class Pm25in(GetJsonApiBase):
+    def __init__(self, token, city, **database):
+        """
+        Init Aqicn:
+            set self.token
+            set self.city
+            create connection with MySQLdb
+            update create table name
+        """
+        super(Pm25in, self).__init__(token, **database)
+        self.city = city
+
+    def handle_data(self, now_time, data):
+        """
+        Input params:
+            city for create table.
+            now_time for newest data.
+            data is a dict, key for create table and value store in table.
+        There two type of data.(each station data and average data)
+        """
+        # example: 2017-03-01T16:00:00Z -> 2017030216(year-mon-day-hour)
+        hour_timestamp = datetime.datetime.strptime(
+            data['time_point'], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y%m%d%H")
+
+        # is newest data, allow 1 hour error
+        if int(now_time) > int(hour_timestamp) + 1:
+            print "Not the newest data: ", data
+
         else:
-            # include time_point
-            _table_items[key] = "varchar(255)"
-    return _table_items
+            timestamp = datetime.datetime.strptime(
+                data['time_point'], "%Y-%m-%dT%H:%M:%SZ").strftime(
+                    "%Y%m%d%H%M%S")
+            data['time_point'] = timestamp
 
-def insert_db(table_name, primary_keys, data):
-    """
-    Insert data in to table.
+            # last data: average city data
+            if data['station_code'] is None and data['position_name'] is None:
+                data['station_code'] = self.city
+                data['position_name'] = self.city
 
-    First, judging whether had created table, if not create it(having primary).
-    Second, it will get the type of data items for creating table.//get_items()
-    After creating table, insert data into table.
+            _orderlist = ['station_code', 'area', 'position_name', 'time_point',
+                          'aqi', 'quality', 'primary_pollutant', 'pm2_5',
+                          'pm2_5_24h', 'pm10', 'pm10_24h', 'no2', 'no2_24h',
+                          'so2', 'so2_24h', 'co', 'co_24h', 'o3', 'o3_8h',
+                          'o3_24h', 'o3_8h_24h']
+            data = self.order_data(data, _orderlist)
 
-    Usage:
-        with db.connection():
-            apidetail.insert_db(table_name, primary_keys, data)
+            _primary_keys = ['station_code', 'time_point']
+            _table_name = self.city.capitalize() + now_time[:-2]  # today
+            self.insert_db(_table_name, _primary_keys, data)
 
-        Using "with db.connection():":
-        Because insert_db do multiply action in one MySQLdb connection.
-    """
-    # has no table and create it.
-    if not db.has_table(table_name):
-        table_items = get_items(data)
-
+    def get_all_data_by_city(self):
+        """
+        input api, city, and token. return json data from api_url.
+        """
+        _api = "http://www.pm25.in/api/querys/aqi_details.json"
         try:
-            db.create_table(table_name, primary_keys, table_items)
+            _all_datas = self._get_api_base(_api, {'city': self.city})
+            # Print sorry message. Cannot get data.
+            if isinstance(_all_datas, dict):
+                for error in _all_datas.itervalues():
+                    print error
+                return []
+            return _all_datas
         except Exception, e:
-            print  'Created table failed. Reason:', e
+            print "not data from %s", _api
+            print e
 
-    try:
-        db.insert(table_name, data)
-    except Exception, e:
-        print 'Inserted data into %s failed.(data: %s)' % (table_name, data)
-        print e
-
-def encode_data(data):
-    """ input dict, encode k & v  utf8 """
-    for k, v in data.iteritems():
-        data.pop(k)
-        k = k.encode('utf-8')
-        if isinstance(v, unicode):
-            data[k] = v.encode('utf-8')
-        else:
-            data[k] = v
-    return data
-
-def order_data(data, orderlist):
-    """
-    Input a dict, and return Orderdict().Order by orderlist.
-    """
-    order_data = OrderedDict()
-    for item in orderlist:
-        order_data[item] = data.get(item)
-    return order_data
-
-def handle_data(city, now_time, data):
-    """
-    Input params:
-        city for create table.
-        now_time for newest data.
-        data is a dict, key for create table and value store in table.
-    There two type of data.(each station data and average data)
-    """
-    # example: 2017-03-01T16:00:00Z -> 2017030216(year-mon-day-hour)
-    timestamp = datetime.datetime.strptime(
-        data['time_point'], "%Y-%m-%dT%H:%M:%SZ").strftime("%Y%m%d%H")
-    data['time_point'] = timestamp
-
-    # is newest data, allow 1 hour error
-    if int(now_time) > int(timestamp) + 1:
-        print "Not the newest data: ", data
-    # newest data, insert to db
-    else:
-
-        # last data: average city data
-        if data['station_code'] is None and data['position_name'] is None:
-            data.pop('station_code')
-            data.pop('position_name')
-
-            orderlist = ['time_point', 'area', 'aqi', 'quality',
-                         'primary_pollutant', 'pm2_5', 'pm2_5_24h', 'pm10',
-                         'pm10_24h', 'no2', 'no2_24h', 'so2', 'so2_24h', 'co',
-                         'co_24h', 'o3', 'o3_8h', 'o3_24h', 'o3_8h_24h']
-            data = order_data(data, orderlist)
-
-            table_name = city.capitalize() + "Average"
-            primary_keys = ['time_point']
-            insert_db(table_name, primary_keys, data)
-        # station data
-        else:
-            orderlist = ['station_code', 'area', 'position_name', 'time_point',
-                         'aqi', 'quality', 'primary_pollutant', 'pm2_5',
-                         'pm2_5_24h', 'pm10', 'pm10_24h', 'no2', 'no2_24h',
-                         'so2', 'so2_24h', 'co', 'co_24h', 'o3', 'o3_8h',
-                         'o3_24h', 'o3_8h_24h']
-            data = order_data(data, orderlist)
-            primary_keys = ['station_code']
-            table_name = city.capitalize() + timestamp
-            insert_db(table_name, primary_keys, data)
-
-
-def _get_api_base(api_url, token, other_params=None):
-    """
-    Base api request function, Return json type data from API.
-    Params is a dict for url params.
-
-    For example:
-        api_url: http://api.com
-        token: xxxxxxx
-        params: {'city': 'guangzhou', 'station': 'no'}
-        And the request url:
-            http://api.com?token=xxxxxxx&city=guangzhou&station=no
-
-    Has no "try, except" syntaxa, so the best USAGE is:
+    def get_data_by_station(self, station_code):
+        """
+        input api, station, and token. return json data from api_url.
+        """
+        _api = "http://www.pm25.in/api/querys/aqis_by_station.json"
         try:
-            _get_api_base()
-        except:
-            ...
-    """
-    params = {'token': token}
-    if other_params is not None:
-        params.update(other_params)
-    url_params = urllib.urlencode(params)
-    url = api_url + '?' + url_params
-    req = urllib2.urlopen(url)
-    json_datas = json.load(req)
-    return json_datas
+            _station_datas = self._get_api_base(_api,
+                                                {'station_code': station_code})
+            return _station_datas
+        except Exception, e:
+            print "not data from %s", _api
 
-def get_api_details_by_city(city, token):
-    """
-    input api, city, and token. return json data from api_url.
-    """
-    api = "http://www.pm25.in/api/querys/aqi_details.json"
-    try:
-        all_datas = _get_api_base(api, token, {'city': city})
-        return all_datas
-    except Exception, e:
-        print "not data from %s", api
-        print e
+    def get_station_list_by_city(self):
+        """
+        Return a city's stations code list.
+        original return data format:
+            {
+              "city": "广州",
+                "stations": [
+                        {
+                                  "station_name": "广雅中学",
+                                  "station_code": "1345A"
+                                },
+                        ...
+                ]
+            }
+        """
+        _station_code_list = []
+        _api = "http://www.pm25.in/api/querys/station_names.json"
+        try:
+            _stations_list = self._get_api_base(_api, {'city': self.city})
+            for _station in _stations_list.get('stations'):
+                _station_code_list.append(
+                    _station['station_code'].encode('utf-8'))
+            return _station_code_list
+        except Exception, e:
+            print "not data from %s", _api
 
-def get_api_details_by_station(station_code, token):
-    """
-    input api, station, and token. return json data from api_url.
-    """
-    api = "http://www.pm25.in/api/querys/aqis_by_station.json"
-    try:
-        station_datas = _get_api_base(api, token,
-                                      {'station_code': station_code})
-        return station_datas
-    except Exception, e:
-        print "not data from %s", api
+    def get_all_data_by_stations(self):
+        """
+        First, get self.city's stations list.
+        Second, get station's data one by one.
+        """
+        _stations_code_list = self.get_station_list_by_city()
+        _all_datas = []
+        for station in _stations_code_list:
+            _station_data = self.get_data_by_station(station)
+            _all_datas = _all_datas + _station_data
+        return _all_datas
 
-def get_station_list_by_city(city, token):
-    """
-    Return a city's stations code list.
-    original return data format:
-        {
-          "city": "广州",
-            "stations": [
-                    {
-                              "station_name": "广雅中学",
-                              "station_code": "1345A"
-                            },
-                    ...
-            ]
-        }
-    """
-    station_code_list = []
-    api = "http://www.pm25.in/api/querys/station_names.json"
-    try:
-        stations_list = _get_api_base(api, token, {'city': city})
-        for station in stations_list.get('stations'):
-            station_code_list.append(station['station_code'].encode('utf-8'))
-        return station_code_list
-    except Exception, e:
-        print "not data from %s", api
+    def run(self):
+        """
+        Two usage:
+            1. Get all city data in one time
+            2. Get data by each station but without average data
+        """
+        # 1. Get all city data in one time
+        all_datas = self.get_all_data_by_city()
 
+        # 2. Get data by each station but without average data
+        # all_datas = self.get_all_data_by_stations()
 
+        if len(all_datas) == 0:
+            print "No json data."
+            return
 
-def main():
-    # connect to database
-    db.create_engine(user='root', password='123456', database='test')
-
-    city = "guangzhou"
-    token = "5j1znBVAsnSf5xQyNQyq"
-
-    # 1. Get all city data in one time
-    # all_datas = get_api_details_by_city(city, token)
-
-    # 2. Get data by each station
-    stations_code_list = get_station_list_by_city(city, token)
-    all_datas = []
-    for station in stations_code_list:
-        station_data = get_api_details_by_station(station, token)
-        all_datas = all_datas + station_data
-
-    # Print sorry message. Cannot get data.
-    if isinstance(all_datas, dict):
-        for v in all_datas.itervalues():
-            print v
-        return
-
-    # print all_datas
-
-    now_time = datetime.datetime.now().strftime("%Y%m%d%H")
-    # print now_time
-    with db.connection():
-        for data in all_datas:
-            # pass Bad station 1347A
-            if data['station_code'] == '1347A':
-                continue
-            handle_data(city, now_time, data)
+        now_time = datetime.datetime.now().strftime("%Y%m%d%H")
+        with connection():
+            for data in all_datas:
+                if data['station_code'] == '1347A': # pass Bad station 1347A
+                    continue
+                self.handle_data(now_time, data)
 
 if __name__ == '__main__':
-    main()
+    city = "guangzhou"
+    token = "5j1znBVAsnSf5xQyNQyq"
+    database = dict(user='root', password='123456', database='test')
+
+    gz = Pm25in(token, city, **database)
+    gz.run()
