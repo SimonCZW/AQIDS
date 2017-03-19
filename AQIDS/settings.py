8350a9a80c70989f922de0d64874f38d33991b64
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-03-19 10:27:51 +0800

    增加APP: mainapp

--- AQIDS/settings.py
+++ AQIDS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4ebuia=o%2^n=06)c3#&@5#b73esp9!6yw@s_i!qy&h6i=5a5*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.101']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
