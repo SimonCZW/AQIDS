24b0c9c975bd24fcd0cf5e3268c102942dde0007
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-03-20 23:55:25 +0800

    根据django重构数据源接口

--- wrapper/jsonapibase.py
+++ wrapper/jsonapibase.py
@@ -4,13 +4,9 @@
 import json
 import urllib
 import urllib2
-from collections import OrderedDict
-
-import db
-
 
 class GetJsonApiBase(object):
-    def __init__(self, token=None, **database):
+    def __init__(self, token=None): #**database)
         """
         Set token and init database.
         Usage:
@@ -19,7 +15,7 @@ class GetJsonApiBase(object):
             >>> GetJsonApiBase(token, **database)
         """
         self.token = token
-        db.create_engine(**database)
+        # db.create_engine(**database)
 
     def get_api_base(self, api_url, other_params=None,
                       headers=None, data=None, json_format=True):
@@ -55,58 +51,3 @@ class GetJsonApiBase(object):
         except Exception, e:
             print "Request %s error." % url,
             print e
-
-    def order_data(self, data, orderlist):
-        """
-        Input a dict, and return Orderdict().Order by orderlist.
-        """
-        order_data = OrderedDict()
-        for item in orderlist:
-            order_data[item] = data.get(item)
-        return order_data
-
-    def insert_db(self, table_name, primary_keys, data):
-        """
-        Insert data in to table.
-
-        First, judging whether had created table, if not create it(having primary).
-        Second, it will get the type of data items for creating table.//get_items()
-        After creating table, insert data into table.
-
-        Usage:
-            with db.connection():
-                apidetail.insert_db(table_name, primary_keys, data)
-
-            Using "with db.connection():":
-            Because insert_db do multiply action in one MySQLdb connection.
-        """
-        # has no table and create it.
-        if not db.has_table(table_name):
-            table_items = self.get_items(data)
-
-            try:
-                db.create_table(table_name, primary_keys, table_items)
-            except Exception, e:
-                print  'Created table failed. Reason:', e
-
-        try:
-            db.insert(table_name, data)
-        except Exception, e:
-            print 'Inserted data into %s failed.(data: %s)' % (table_name, data)
-            print e
-
-    def get_items(self, data):
-        """
-        return table items from data for create table.
-        """
-        _table_items = OrderedDict()
-        for key, value in data.iteritems():
-            if isinstance(key, unicode):
-                key = key.encode('utf-8')
-            if isinstance(value, int):
-                _table_items[key] = "int"
-            elif isinstance(value, float):
-                _table_items[key] = "float"
-            else:
-                _table_items[key] = "varchar(255)"
-        return _table_items
