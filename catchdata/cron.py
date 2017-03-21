7c39f21bb515211b6e343d87803cce0040d7d3cf
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-03-21 15:08:46 +0800

    构建了单独获取aqicn/gzepb的station数据接口,aqi数据接口结构

--- catchdata/cron.py
+++ catchdata/cron.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import datetime
 
 # standalone init
 # import django
@@ -21,50 +22,88 @@ if BASE_DIR not in sys.path:
 from gzepb.gzepb_api import Gzepb
 from aqicn.aqicn_api import Aqicn
 
-def get_and_store_aqicn():
-    print "start get and store aqicn..."
-    token = 'de552bc7dcefe469e68466f97d31f0c88faef2c1'
-    gz_aqicn = Aqicn(token)
+_AQICN_STATIONS_LIST = ['/guangzhou/',
+                        '/guangdong/guangzhou/us-consulate/',
+                        '/guangdong/guangzhou/tiyuxi/',
+                        '/guangdong/guangzhou/modieshazhan/',
+                        '/guangdong/guangzhou/haizhuchisha/',
+                        '/guangdong/guangzhou/haizhubaogang/',
+                        '/guangdong/guangzhou/gongyuanqian/',
+                        '/guangdong/guangzhou/liwanxicun/',
+                        '/guangdong/guangzhou/luhu/',
+                        '/guangdong/guangzhou/huaduchengqu/',
+                        '/guangdong/guangzhou/zhudong/',
+                        '/guangdong/guangzhou/tianhu/',
+                        '/guangdong/guangzhou/luogangzhenlong/',
+                        '/guangdong/guangzhou/huangpudashadi/',
+                        '/guangdong/guangzhou/fanyushiqiao/',
+                        '/guangdong/guangzhou/wanqingsha/']
 
-    stations_url_list = ['/guangzhou/',
-                         '/guangdong/guangzhou/us-consulate/',
-                         '/guangdong/guangzhou/tiyuxi/',
-                         '/guangdong/guangzhou/modieshazhan/',
-                         '/guangdong/guangzhou/haizhuchisha/',
-                         '/guangdong/guangzhou/haizhubaogang/',
-                         '/guangdong/guangzhou/gongyuanqian/',
-                         '/guangdong/guangzhou/liwanxicun/',
-                         '/guangdong/guangzhou/luhu/',
-                         '/guangdong/guangzhou/huaduchengqu/',
-                         '/guangdong/guangzhou/zhudong/',
-                         '/guangdong/guangzhou/tianhu/',
-                         '/guangdong/guangzhou/luogangzhenlong/',
-                         '/guangdong/guangzhou/huangpudashadi/',
-                         '/guangdong/guangzhou/fanyushiqiao/',
-                         '/guangdong/guangzhou/wanqingsha/']
-    gz_aqicn.run(stations_url_list)
-    # gz_aqicn.aqi_datas
-    for station in gz_aqicn.station_datas:
-        print station
+def get_and_store_aqicn_aqi():
+    print "%s Start get and store aqicn aqi datas..." % str(
+        datetime.datetime.now())
+    aqi_datas = Aqicn.get_aqi_data(_AQICN_STATIONS_LIST)
+    for aqi_data in aqi_datas:
+        pass
+        # do_something before store into database:
+        # - foreign key
+
+def get_and_store_aqicn_station():
+    print "%s Start get and store aqicn station datas..." % str(
+        datetime.datetime.now())
+    station_datas = Aqicn.get_station_data(_AQICN_STATIONS_LIST)
+    for station in station_datas:
+        print "%s Trying to insert aqicn stations data:" % str(
+            datetime.datetime.now()), station
         try:
             Station.objects.create(**station)
             Station.objects.save()
         except Exception, e:
-            print e
+            print "%s Error: insert aqicn stations data:" % str(
+                datetime.datetime.now()), station, e
 
-def get_and_store_gzepb():
-    pass
+def get_and_store_gzepb_aqi():
+    """
+    aqi data.
+    """
+    print "%s Start get and store gzepb aqi datas..." % str(
+        datetime.datetime.now())
+    aqi_datas = Gzepb.get_aqi_data()
+    for aqi_data in aqi_datas:
+        # do_something before store into database:
+        # - foreign key
+        # - message
+        pass
+
+def get_and_store_gzepb_station():
+    """
+    station data.
+    """
+    print "%s Start get and store gzepb station's datas..." % str(
+        datetime.datetime.now())
+    station_datas = Gzepb.get_station_data()
+    for station in station_datas:
+        print "%s Trying to insert gzepb stations data:" % str(
+            datetime.datetime.now()), station
+        try:
+            Station.objects.create(**station)
+            Station.objects.save()
+        except Exception, e:
+            print "%s Error: insert gzepb stations data:" % str(
+                datetime.datetime.now()), station, e
 
 def test_station():
-    station_data_example = {'station_name': 'ss', 'station_type': 'evaluate_point', 'city': 'gz'}
+    station_data_example = {'station_name': 'x', 'station_type': '国控点', 'city': 'gz'}
     Station.objects.create(**station_data_example)
     Station.objects.save()
+    # print Station.objects.filter(station_type='国控')
 
 def main():
    print ChinaStandard.objects.all()
 
 if __name__ == '__main__':
-    # pass
+    pass
     # main()
     # test_station()
-    get_and_store_aqicn()
+    # get_and_store_aqicn_station()
+    # get_and_store_gzepb_station()
