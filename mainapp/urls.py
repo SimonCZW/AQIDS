21f169e8aa2be35585cbf2f7c65dc8a545d9707c
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-04-04 14:10:31 +0800

    增加测试页面

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'mainapp'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^test/$', views.TestIndexView.as_view(), name='indextest'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -30,3 +30,29 @@ class IndexView(TemplateView):
                 time_point=hour_ago)
 
         return context
+
+class TestIndexView(TemplateView):
+    template_name = "mainapp/index.html.bak"
+
+    def get_context_data(self, **kwargs):
+        context = super(TestIndexView, self).get_context_data(**kwargs)
+
+        hour_ago = (datetime.datetime.now()
+                    - datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:00:00")
+        hour_now = (datetime.datetime.now()
+                    - datetime.timedelta(hours=1)).strftime("%Y-%m-%d %H:00:00")
+        if GzepbAqiData.objects.filter(time_point=hour_now).exists():
+            context['lastest_gzepb_data'] = GzepbAqiData.objects.filter(
+                time_point=hour_now)
+        elif GzepbAqiData.objects.filter(time_point=hour_ago).exists():
+            context['lastest_gzepb_data'] = GzepbAqiData.objects.filter(
+                time_point=hour_ago)
+
+        if AqicnIAqiData.objects.filter(time_point=hour_now).exists():
+            context['lastest_aqicn_data'] = AqicnIAqiData.objects.filter(
+                time_point=hour_now)
+        elif AqicnIAqiData.objects.filter(time_point=hour_ago).exists():
+            context['lastest_aqicn_data'] = AqicnIAqiData.objects.filter(
+                time_point=hour_ago)
+
+        return context
