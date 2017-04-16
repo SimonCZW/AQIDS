2f586000e19331e2745fcb2b2a460f34bdfc4a65
SimonCZW <chenzhuowen.simon@foxmail.com>
2017-04-16 17:20:00 +0800

    新增将gzepb数据转换为美国标准存储新的model

--- catchdata/cron.py
+++ catchdata/cron.py
@@ -14,6 +14,9 @@ import datetime
 
 from mainapp.models import AqiStandard, Station
 from mainapp.models import GzepbAqiData, AqicnIAqiData
+from mainapp.models import GzepbAqiDataUsaStandard
+
+import aqi
 
 # import custom get data object
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,6 +122,57 @@ def get_and_store_aqicn_station():
             print "%s Error: insert aqicn stations data:" % str(
                 datetime.datetime.now()), station, e
 
+
+def get_and_store_gzepb_aqi_usastandard(fk):
+    """
+    transfer usa standard
+    """
+    # transfer usa standard
+    # aqi , dominentpol, quality, so2/no2/pm20/co/o3/pm25_iaqi
+    usa_standard_data = {}
+    usa_standard_data['pm25_iaqi'] = aqi.to_iaqi(aqi.POLLUTANT_PM25, fk.pm25_1h)
+    usa_standard_data['pm10_iaqi'] = aqi.to_iaqi(aqi.POLLUTANT_PM10, fk.pm10_1h)
+    usa_standard_data['so2_iaqi'] = aqi.to_iaqi(aqi.POLLUTANT_SO2_1H, fk.so2_1h)
+    usa_standard_data['no2_iaqi'] = aqi.to_iaqi(aqi.POLLUTANT_NO2_1H, fk.no2_1h)
+    usa_standard_data['co_iaqi'] = aqi.to_iaqi(aqi.POLLUTANT_CO_8H, fk.co_1h)
+    usa_standard_data['o3_iaqi_8h'] = aqi.to_iaqi(aqi.POLLUTANT_O3_8H, fk.o3_1h)
+
+
+    aqi_value = 0
+    dominentpol = None
+    for k, v in usa_standard_data.iteritems():
+        if v >= aqi_value:
+            aqi_value = v
+            dominentpol = k
+    usa_standard_data['dominentpol'] = dominentpol.split('_')[0].upper()
+    usa_standard_data ['aqi'] = aqi_value
+
+    _data = GzepbAqiDataUsaStandard(**usa_standard_data)
+    _data.origin_data = fk
+    _data.station_name = fk.station_name
+    _data.time_point = fk.time_point
+    _data.date = fk.date
+
+    # _data.quality = AqiStandard.objects.get(usa_quality='中等')
+    if 0 <= aqi_value <= 50:
+        _data.quality = AqiStandard.objects.get(usa_quality='好')
+    elif 51 <= aqi_value <= 100:
+        _data.quality = AqiStandard.objects.get(usa_quality='中等')
+    elif 101 <= aqi_value <= 150:
+        _data.quality = AqiStandard.objects.get(usa_quality='对敏感人群不健康')
+    elif 151 <= aqi_value <= 200:
+        _data.quality = AqiStandard.objects.get(usa_quality='不健康')
+    elif 201 <= aqi_value <= 300:
+        _data.quality = AqiStandard.objects.get(usa_quality='非常不健康')
+    elif aqi_value >= 301:
+        _data.quality = AqiStandard.objects.get(usa_quality='危险')
+    else:
+        _data.quality = AqiStandard.objects.get(usa_quality='空')
+
+    print "%s Start get and store gzepb aqi datas(usa standard)..." % str(
+        datetime.datetime.now())
+    _data.save()
+
 def get_and_store_gzepb_aqi():
     """
     Gzepb aqi data.
@@ -146,6 +200,10 @@ def get_and_store_gzepb_aqi():
             _aqicn_data.quality = AqiStandard.objects.get(
                 china_quality=_quality)
             _aqicn_data.save()
+
+            # transfer and save usa standard iaqi and aqi
+            get_and_store_gzepb_aqi_usastandard(fk=_aqicn_data)
+
         else:
             print "%s GzepbAqiData already exists: " % str(
                 datetime.datetime.now()), aqi_data
@@ -172,6 +230,8 @@ def get_and_store_gzepb_station():
         except Exception, e:
             print "%s Error: insert gzepb stations data:" % str(
                 datetime.datetime.now()), station, e
+
+
 
 
 if __name__ == '__main__':
